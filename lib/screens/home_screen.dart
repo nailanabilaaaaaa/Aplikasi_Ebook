3b1c2c9f3ac390396cd889f8b8eab0f5d1600834
nailanabilaaaaaa <nailanabilaaaaaa@gmail.com>
2021-11-26 09:47:20 +0700

    Update Halaman Home

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -73,13 +73,105 @@ class HomeScreen extends StatelessWidget {
             Padding(
               padding: EdgeInsets.symmetric(horizontal: 24),
               child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
                 children: <Widget>[
                   RichText(
                     text: TextSpan(
                       style: Theme.of(context).textTheme.headline4,
-                      children: [TextSpan(text: "Best of the")],
+                      children: [TextSpan(text: "Best of the "),
+                      TextSpan(
+                        text: "day", style: TextStyle(fontWeight: FontWeight.bold),
+                        ),
+                      ],
                     ),
                   ),
+                  Container(
+                    margin: EdgeInsets.symmetric(vertical: 20),
+                    width: double.infinity,
+                    height: 205,
+                    child: Stack(
+                      children: <Widget>[
+                        Positioned(
+                          bottom: 0,
+                          left: 0,
+                          right: 0,
+                          child: Container(
+                            padding: EdgeInsets.only(
+                              left: 24, 
+                              top: 24, 
+                              right: size.width * .35,
+                            ),
+                            height: 185,
+                            width: double.infinity,
+                            decoration: BoxDecoration(
+                              color: Color(0xFFEAEAEA).withOpacity(.45),
+                              borderRadius: BorderRadius.circular(29),
+                            ),
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: <Widget>[
+                                Text(
+                                  "New York Time Best For 11th March 2020",
+                                  style: TextStyle(
+                                    fontSize: 9,
+                                    color: kLightBlackColor,
+                                  ),
+                                ),
+                                SizedBox(height: 5),
+                                Text(
+                                  "How To Win \nFriends & Influence",
+                                  style: Theme.of(context).textTheme.headline6,
+                                ),
+                                Text(
+                                  "Gary Venchuk", 
+                                  style: TextStyle(color: kLightBlackColor),
+                                ),
+                                SizedBox(height: 10),
+                                Row(
+                                  children: <Widget>[
+                                    BookRating(score: 4.9),
+                                    SizedBox(width: 10),
+                                    Expanded(
+                                      child: Text(
+                                        "When the earth was flat and everyone wanted to win the game of the best and people...",
+                                        maxLines: 3,
+                                        overflow: TextOverflow.ellipsis,
+                                        style: TextStyle(
+                                          fontSize: 10,
+                                          color: kLightBlackColor,
+                                        ),
+                                      ),
+                                    )
+                                  ],
+                                ),
+                              ],
+                            ),
+                          ),
+                        ),
+                        Positioned(
+                          right: 0,
+                          top: 0,
+                          child: Image.asset(
+                            "assets/images/book-3.png", 
+                            width: size.width * .37,
+                          ),
+                        ),
+                        Positioned(
+                          bottom: 0,
+                          right: 0,
+                          child: SizedBox(
+                            height: 40,
+                            width: size.width * .3,
+                            child: TwoSideRoundedButton(
+                              text: "Read",
+                              radious: 24,
+                              press: () {},
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  )
                 ],
               ),
             ),
