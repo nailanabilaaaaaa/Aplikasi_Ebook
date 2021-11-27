c30928806e4a18ca422e0f33ea7589efdd50c210
ahmadhafis <ahmadhafis296@gmail.com>
2021-11-27 19:28:40 +0700

    Progress HomeScreen

--- lib/main.dart
+++ lib/main.dart
@@ -76,7 +76,7 @@ class WelcomeScreen extends StatelessWidget {
                       ),
                     );
                   },
-                )
+                ),
             ),
           ],
         ),
