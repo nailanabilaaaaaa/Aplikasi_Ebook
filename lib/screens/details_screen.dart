be9ea2a219836c153d81d793ffe267d7cc883228
SriKynanti <kynanti.ti@gmail.com>
2021-11-28 20:51:07 +0700

    Update DetailScreen - BookInfo

--- lib/screens/details_screen.dart
+++ lib/screens/details_screen.dart
@@ -3,6 +3,7 @@
 import 'package:ebook_app/consttans.dart';
 import 'package:ebook_app/widgets/book_rating.dart';
 import 'package:ebook_app/widgets/rounded_button.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class DetailsScreen extends StatelessWidget {
@@ -38,67 +39,17 @@ class DetailsScreen extends StatelessWidget {
                         SizedBox(
                           height: size.height * .1,
                         ),
-                        Row(
-                          children: <Widget>[
-                            Expanded(
-                              child: Column(
-                                crossAxisAlignment: CrossAxisAlignment.start,
-                                children: <Widget>[
-                                  Text(
-                                    "Crushing &",
-                                    style:
-                                        Theme.of(context).textTheme.headline4,
-                                  ),
-                                  Text(
-                                    "Influence",
-                                    style: Theme.of(context)
-                                        .textTheme
-                                        .headline4
-                                        ?.copyWith(fontWeight: FontWeight.bold),
-                                  ),
-                                  Row(
-                                    children: <Widget>[
-                                      Expanded(
-                                        child: Column(
-                                          // ignore: prefer_const_literals_to_create_immutables
-                                          children: <Widget>[
-                                            Text(
-                                              "Ini untuk deskripsi buku",
-                                              maxLines: 5,
-                                              style: TextStyle(
-                                                fontSize: 12,
-                                                color: kLightBlackColor,
-                                              ),
-                                            ),
-                                            SizedBox(
-                                              height: 5,
-                                            ),
-                                            RoundedButton(text: "Read", fontSize: 12,verticalPadding: 10, press: (){}),
-                                          ],
-                                        ),
-                                      ),
-                                      Column(
-                                        children: <Widget>[
-                                          IconButton(
-                                            onPressed: () {},
-                                            icon: Icon(Icons.favorite_border),
-                                          ),
-                                          BookRating(score: 4.9)
-                                        ],
-                                      ),
-                                    ],
-                                  ),
-                                ],
-                              ),
-                            ),
-                            Image.asset(
-                              "assets/images/book-1.png",
-                              height: 200,
-                            ),
-                          ],
-                        ),
+                        BookInfo(),
                       ],
                     ),
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.only(top: size.height * .4 - 30),
+                  child: Column(
+                    children: <Widget>[
+                      ChapterCard(size: size),
+                    ],
                   ),
                 ),
               ],
@@ -106,6 +57,140 @@ class DetailsScreen extends StatelessWidget {
           ],
         ),
       ),
+    );
+  }
+}
+
+class ChapterCard extends StatelessWidget {
+  const ChapterCard({
+    Key? key,
+    required this.size,
+  }) : super(key: key);
+
+  final Size size;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: 
+        EdgeInsets.symmetric(vertical: 20, horizontal: 30),
+      margin: EdgeInsets.only(bottom: 16),
+      width: size.width - 48,
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(38.5),
+        boxShadow: [
+          BoxShadow(
+            offset: Offset(0, 10),
+            blurRadius: 33,
+            color: Color(0xFFD3D3D3).withOpacity(.84),
+          ),
+        ],
+      ),
+      child: Row(
+        children: <Widget>[
+          RichText(
+            text: TextSpan(
+              // ignore: prefer_const_literals_to_create_immutables
+              children: [
+                TextSpan(
+                  text: "Chapter 1 : Money \n",
+                  style: TextStyle(
+                    fontSize: 16,
+                    color: kBlackColor,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                TextSpan(
+                  text: "Life is about change", 
+                  style: TextStyle(color: kLightBlackColor),
+                ),
+              ],
+            ),
+          ),
+          Spacer(),
+          IconButton(
+            icon: Icon(
+              Icons.arrow_forward_ios, 
+              size: 18,
+            ), 
+            onPressed: () {},
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class BookInfo extends StatelessWidget {
+  const BookInfo({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: <Widget>[
+        Expanded(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Text(
+                "Crushing &",
+                style:
+                    Theme.of(context).textTheme.headline4,
+              ),
+              Text(
+                "Influence",
+                style: Theme.of(context)
+                    .textTheme
+                    .headline4
+                    ?.copyWith(fontWeight: FontWeight.bold),
+              ),
+              SizedBox(
+                height: 5,
+              ),
+              Row(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Expanded(
+                    child: Column(
+                      // ignore: prefer_const_literals_to_create_immutables
+                      children: <Widget>[
+                        Text(
+                          "Ini untuk deskripsi buku",
+                          maxLines: 5,
+                          style: TextStyle(
+                            fontSize: 12,
+                            color: kLightBlackColor,
+                          ),
+                        ),
+                        SizedBox(
+                          height: 5,
+                        ),
+                        RoundedButton(text: "Read", fontSize: 12,verticalPadding: 10, press: (){}),
+                      ],
+                    ),
+                  ),
+                  Column(
+                    children: <Widget>[
+                      IconButton(
+                        onPressed: () {},
+                        icon: Icon(Icons.favorite_border),
+                      ),
+                      BookRating(score: 4.9)
+                    ],
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+        Image.asset(
+          "assets/images/book-1.png",
+          height: 200,
+        ),
+      ],
     );
   }
 }
