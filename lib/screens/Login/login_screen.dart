6de0c8ce4d88f57a061b68ed0215e51785526471
nailanabilaaaaaa <nailanabilaaaaaa@gmail.com>
2021-12-03 22:53:26 +0700

    Add Login Screen

---
+++ lib/screens/Login/login_screen.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:ebook_app/screens/Login/components/body.dart';
+
+class LoginScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Body(),
+    );
+  }
+}
